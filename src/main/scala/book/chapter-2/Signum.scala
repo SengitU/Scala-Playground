0306c9bc02220d4ff764f6cb28fa428d662a9a1e
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-03-05 14:35:41 +0100

    Completed question #1

--- src/main/scala/book/chapter-2/Signum.scala
+++ src/main/scala/book/chapter-2/Signum.scala
@@ -1,7 +1,5 @@
 package book
 
 object Signum {
-  def getSign(x: Int) = {
-    1
-  }
+  def getSign(x: Int) = if(x == 0) 0 else if(x > 1) 1 else -1
 }
--- src/test/scala/book/chapter-2/SignumSpec.scala
+++ src/test/scala/book/chapter-2/SignumSpec.scala
@@ -6,4 +6,12 @@ class SignumSpec extends FlatSpec with Matchers {
   "Signum spec" should "return 1 for positive number" in {
     Signum.getSign(20) shouldEqual 1
   }
+
+  "Signum spec" should "return -1 for negative number" in {
+    Signum.getSign(-20) shouldEqual -1
+  }
+
+  "Signum spec" should "return 0 for 0" in {
+    Signum.getSign(0) shouldEqual 0
+  }
 }
