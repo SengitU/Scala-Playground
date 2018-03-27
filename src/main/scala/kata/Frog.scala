5bd9b663d2e3e9640d71731b69e004e4b04c6df9
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-03-27 10:06:36 +0200

    Practiced frog spec

--- src/main/scala/kata/Frog.scala
+++ src/main/scala/kata/Frog.scala
@@ -3,5 +3,13 @@ package kata
 // Detailed Kata Explanation https://github.com/pinarkaymaz6/FrogRiverOne
 
 object Frog {
-  def crossedIndex(i: Int, ints: Array[Int]) = 1
+  def crossedIndex(distance: Int, leaves: Array[Int]): Int = {
+    var temp = -1
+    for(number <- 1 to distance) {
+      val indexOfNumber = leaves.indexOf(number)
+      if(indexOfNumber == -1) return -1
+      if(indexOfNumber > temp) temp = indexOfNumber + 1
+    }
+    temp
+  }
 }
--- src/test/scala/kata/FrogSpec.scala
+++ src/test/scala/kata/FrogSpec.scala
@@ -6,4 +6,12 @@ class FrogSpec extends FlatSpec with Matchers {
   "crossedIndex" should "1, [1] => 1" in {
     Frog.crossedIndex(1, Array(1)) should equal(1)
   }
+
+  "crossedIndex" should "2, [1, 1, 1, 2] => 4" in {
+    Frog.crossedIndex(2, Array(1, 1, 1, 2)) should equal(4)
+  }
+
+  "crossedIndex" should "2, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] => -1" in {
+    Frog.crossedIndex(2, Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)) should equal(-1)
+  }
 }
